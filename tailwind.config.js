3c70fa6a370e6c19c6f2e20e91a168461794a8e7
Kodanda <kodandareddy21@gmail.com>
2022-08-17 15:36:34 +0530

    shopify completed

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+       cream:"#FBF7ED",
+       liteBlack:"#42474c",
+       green:"#008060",
+       darkGreen:"#004C3F",
+       brown:"#6b7177",
+       liteGreen:"#c1f0d0",
+       paleGreen:"#f3fcf4",
+       gray:"#b6babf"
+      }
+    },
+  },
+  plugins: [],
+}
